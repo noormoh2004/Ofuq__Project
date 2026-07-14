b6165173e10a08b672067bc6d272cddccf0db791
noormoh2004 <noormughari10@gmail.com>
2026-07-15 01:48:01 +0300

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
+    implementation ("com.airbnb.android:lottie:6.0.0")
 }
