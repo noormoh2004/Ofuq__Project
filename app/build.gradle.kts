b84c78f2619b683a88dc942ca4f8d328b58eb6d4
noormoh2004 <noormughari10@gmail.com>
2026-07-16 12:58:23 +0300

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("com.airbnb.android:lottie:6.0.0")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
