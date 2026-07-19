63ca6e5356b665b14c8eaf8199ded4caf7f8d944
noormoh2004 <noormughari10@gmail.com>
2026-07-19 16:47:03 +0300

    updates

--- app/src/main/java/com/example/ofuqproject/stories_screen.java
+++ app/src/main/java/com/example/ofuqproject/stories_screen.java
@@ -14,7 +14,7 @@ public class stories_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_stories_screen);
+        setContentView(R.layout.activity_choice_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
